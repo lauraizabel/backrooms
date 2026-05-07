ffcf9277cff10ed968f8bbff378e24e854962676
lauraizabel <lauraizabel8569@gmail.com>
2026-05-07 18:19:27 -0300

    Fix: instala Fabric Server antes dos mods

--- start.sh
+++ start.sh
@@ -4,6 +4,9 @@ set -e
 SERVER_DIR="server"
 mkdir -p "$SERVER_DIR" && cd "$SERVER_DIR"
 
+MC_VERSION="1.20.1"
+FABRIC_INSTALLER_VERSION="1.0.1"
+
 # Baixa o mrpack-install se ainda nao tiver
 if [ ! -f mrpack-install ]; then
   echo "Baixando mrpack-install..."
@@ -11,9 +14,17 @@ if [ ! -f mrpack-install ]; then
   chmod +x mrpack-install
 fi
 
-# Instala o modpack do Modrinth (Into the Backrooms: Found Footage)
+# Instala o Fabric Server se ainda nao tiver
 if [ ! -f fabric-server-launch.jar ]; then
-  echo "Instalando modpack..."
+  echo "Instalando Fabric Server ${MC_VERSION}..."
+  wget -q -O fabric-installer.jar "https://maven.fabricmc.net/net/fabricmc/fabric-installer/${FABRIC_INSTALLER_VERSION}/fabric-installer-${FABRIC_INSTALLER_VERSION}.jar"
+  java -jar fabric-installer.jar server -mcversion ${MC_VERSION} -downloadMinecraft
+  rm fabric-installer.jar
+fi
+
+# Instala os mods do modpack se ainda nao tiver
+if [ ! -d mods ] || [ -z "$(ls -A mods 2>/dev/null)" ]; then
+  echo "Instalando mods do modpack..."
   ./mrpack-install modrinth into-the-backrooms-found-footage --server-dir .
 fi
 
